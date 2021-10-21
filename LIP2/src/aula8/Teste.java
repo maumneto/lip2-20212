0e3865e2ebb5b69e09440e4b4fda2466e7d625c9
Maurí­cio Moreira Neto <maumneto@gmail.com>
2021-10-21 17:56:18 -0300

    new codes of aula8

---
+++ LIP2/src/aula8/Teste.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula8;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author mauricio.moreira
+ */
+
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Olá!");
+        Scanner in = new Scanner(System.in);
+        Pattern pattern;
+        aula8.Circulo circulo = new aula8.Circulo();
+    }
+}
---
+++ LIP2/src/aula8/exercicio1/TestPackage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula8.exercicio1;
+
+import aula8.Circulo;
+import static java.lang.Math.*;
+/**
+ *
+ * @author mauricio.moreira
+ */
+public class TestPackage {
+    public static void main(String[] args) {
+        Circulo circ = new Circulo();
+        sqrt(9);
+    }
+}
